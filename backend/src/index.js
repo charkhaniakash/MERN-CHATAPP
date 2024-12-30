cc42dfcef75dda7cc27c1f922a04739d271300da
charkhaniakash <charkhaniakash@gmail.com>
2024-12-31 02:13:55 +0530

    craeted starter files and installed required libraries

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+// type= !module
+// const express = require("express");
+// type= module
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5001, () => {
+  console.log("Server is running on port 3000");
+});
