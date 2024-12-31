df41b1ed33d61efef418160749382a918c5f95a6
charkhaniakash <charkhaniakash@gmail.com>
2024-12-31 15:43:09 +0530

    done with signup api

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,4 +1,7 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import {generateToken} from "../lib/utils.js";
+
 
 export const signup = async(req, res) => {
   const { fullName, email, password } = req.body;
@@ -8,7 +11,6 @@ export const signup = async(req, res) => {
     }
     const user = await User.findOne({email});
     if(user) return res.status(400).json({message:"User already exists"});
-
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password,salt);
     const newUser = new User(
@@ -18,6 +20,7 @@ export const signup = async(req, res) => {
             password:hashPassword
         }
     );
+
 
     if(newUser){
         generateToken(newUser._id,res);
--- backend/src/index.js
+++ backend/src/index.js
@@ -11,9 +11,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT; 
 
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
