ac7fbfd9e22ff05842b5247998f72565faf37966
charkhaniakash <charkhaniakash@gmail.com>
2025-01-07 00:57:08 +0530

    sidebar ui updated

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -28,9 +28,9 @@ const Sidebar = () => {
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <Users className="size-6" />
-          <span className="font-medium hidden lg:block">Contacts</span>
+          <span className="font-medium lg:block">Contacts</span>
         </div>
-        <div className="mt-3 hidden lg:flex items-center gap-2">
+        <div className="mt-3 lg:flex items-center gap-2">
           <label className="cursor-pointer flex items-center gap-2">
             <input
               type="checkbox"
@@ -49,7 +49,7 @@ const Sidebar = () => {
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <Users className="size-6" />
-          <span className="font-medium hidden lg:block">Contacts</span>
+          <span className="font-medium lg:block">Contacts</span>
         </div>
       </div>
       <div className="overflow-y-auto w-full py-3">
@@ -88,7 +88,7 @@ const Sidebar = () => {
             </div>
 
             {/* User info visible only on larger screens */}
-            <div className="hidden lg:block text-left min-w-0">
+            <div className="lg:block text-left min-w-0">
               <div className="font-medium truncate">{user.fullName}</div>
               {/* <div className="text-sm text-zinc-400">
                 {connectedUsers.includes(user._id) ? "Online" : "Offline"}
