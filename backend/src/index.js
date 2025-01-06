272d43c85bf96a569a8a642b0faa239a38f055ed
charkhaniakash <charkhaniakash@gmail.com>
2025-01-06 18:59:33 +0530

    updated the env url and dev url

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,9 +9,12 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import { app ,server  } from "./lib/socket.js";
+import path from "path";
 
 
 dotenv.config();
+const __dirname = path.resolve()
+
 const PORT = process.env.PORT; 
 
 app.use(express.json({ limit: '50mb' }));
@@ -27,6 +30,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV  === 'production'){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"../frontend/dist/index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,10 @@
 
 import axios from "axios";
 
-
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000/api",
-    withCredentials:true,
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "/api",
+  withCredentials: true,
+});
