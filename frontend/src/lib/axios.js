05559f412dc86530174d8bd9ea311917ef3fea72
charkhaniakash <charkhaniakash@gmail.com>
2025-01-06 20:35:13 +0530

    configurations changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,10 +2,8 @@
 
 import axios from "axios";
 
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "/api",
-  withCredentials: true,
-});
+    baseURL:"http://localhost:8080/api",
+    withCredentials:true,
+})
